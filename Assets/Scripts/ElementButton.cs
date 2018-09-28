0868a14d31060827d9a33656f7da5a25e24a8eb8
wytriam <thomas.m.frick@gmail.com>
2018-09-28 15:13:17 -0400

    have a grid made of UI buttons that can be navigated with arrow keys/WASD

---
+++ Assets/Scripts/ElementButton.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementButton : MonoBehaviour
+{
+    [Header("Neighboring UI Elements - Set Dynamically")]
+    public GameObject northNeighbor;
+    public GameObject eastNeighbor;
+    public GameObject southNeighbor;
+    public GameObject westNeighbor;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        FindNeighbors();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void PrintButtonClick()
+    {
+        Debug.Log("ElementButton::PrintButtonClick() - " + gameObject.name + " was clicked");
+    }
+
+    // Find the neighboring UI elements dynamically through raycasts (won't find UI elements without a collider)
+    public void FindNeighbors()
+    {
+        // clear out old neighbors
+        eastNeighbor = null;
+        westNeighbor = null;
+        northNeighbor = null;
+        southNeighbor = null;
+
+        // set up raycast
+        Ray ray;
+        RaycastHit info;
+
+        // Look to the right (east)
+        ray = new Ray(transform.position, new Vector3(1, 0));
+        Physics.Raycast(ray, out info);
+        if (info.collider != null)
+        {
+            //Debug.Log("ElementButton::FindNeighbors() - Neighbor found to the east! - " + info.collider.gameObject.name);
+            eastNeighbor = info.collider.gameObject;
+        }
+        else
+        {
+            //Debug.Log("ElementButton::FindNeighbors() - No neighbor found to the east");
+        }
+
+        // look to the left (west)
+        ray = new Ray(transform.position, new Vector3(-1, 0));
+        Physics.Raycast(ray, out info);
+        if (info.collider != null)
+        {
+            //Debug.Log("ElementButton::FindNeighbors() - Neighbor found to the west! - " + info.collider.gameObject.name);
+            westNeighbor = info.collider.gameObject;
+        }
+        else
+        {
+            //Debug.Log("ElementButton::FindNeighbors() - No neighbor found to the west");
+        }
+
+        // look to the up (north)
+        ray = new Ray(transform.position, new Vector3(0, 1));
+        Physics.Raycast(ray, out info);
+        if (info.collider != null)
+        {
+            //Debug.Log("ElementButton::FindNeighbors() - Neighbor found to the north! - " + info.collider.gameObject.name);
+            northNeighbor = info.collider.gameObject;
+        }
+        else
+        {
+            //Debug.Log("ElementButton::FindNeighbors() - No neighbor found to the north");
+        }    
+            
+        // look to the down (south)
+        ray = new Ray(transform.position, new Vector3(0, -1));
+        Physics.Raycast(ray, out info);
+        if (info.collider != null)
+        {
+            //Debug.Log("ElementButton::FindNeighbors() - Neighbor found to the south! - " + info.collider.gameObject.name);
+            southNeighbor = info.collider.gameObject;
+        }
+        else
+        {
+            //Debug.Log("ElementButton::FindNeighbors() - No neighbor found to the south");
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI_InputMan.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_InputMan : MonoBehaviour
+{
+    [Header("Colors for UI Grid Elements")]
+    public Color defaultColor;   // the color for non-active, non-selected UI elements
+    public Color selectedColor;  // the color for non-active, selected UI elements
+    public Color activeColor;    // the color for active UI elements
+
+    [Header("GameObject the Player is currently on")]
+    public GameObject selectedGO;
+    [Header("GameObject the Player has pressed a button on")]
+    public GameObject activeGO;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        SelectElement(selectedGO);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.JoystickButton3))
+            SelectElement(selectedGO.GetComponent<ElementButton>().northNeighbor);
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.JoystickButton2))
+            SelectElement(selectedGO.GetComponent<ElementButton>().eastNeighbor);
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.JoystickButton0))
+            SelectElement(selectedGO.GetComponent<ElementButton>().southNeighbor);
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.JoystickButton1))
+            SelectElement(selectedGO.GetComponent<ElementButton>().westNeighbor);
+
+    }
+
+    void SelectElement(GameObject newElement)
+    {
+        if (newElement == null) return;
+        selectedGO.GetComponent<Image>().color = defaultColor;
+        selectedGO = newElement;
+        selectedGO.GetComponent<Image>().color = selectedColor;
+
+    }
+}
