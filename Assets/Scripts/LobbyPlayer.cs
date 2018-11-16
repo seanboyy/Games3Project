924b65255a09e54b42618bb2fbe1d66c94ffc978
BuildTools <sbamford12@gmail.com>
2018-11-16 10:53:20 -0500

    Let's see if this works

--- Assets/Scripts/LobbyPlayer.cs
+++ Assets/Scripts/LobbyPlayer.cs
@@ -5,15 +5,18 @@ using UnityEngine.Networking;
 
 public class LobbyPlayer : NetworkBehaviour {
 
+    public static NetworkLobbyPlayer lobbyPlayer;
+    public NetworkLobbyManager lobbyManager;
     public LobbyMan lobbyMan;
 
 	// Use this for initialization
 	void Start () {
         lobbyMan = FindObjectOfType<LobbyMan>();
+        if (isLocalPlayer) lobbyPlayer = GetComponent<NetworkLobbyPlayer>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+        if (isLocalPlayer) (lobbyManager.OnLobbyServerCreateGamePlayer(connectionToClient, playerControllerId)).GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
 	}
 }
